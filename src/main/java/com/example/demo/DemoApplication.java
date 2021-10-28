0a5296d99c1b7cd78257f12158a45d7a72f01fb9
Gaddi <77565717+RYGJR@users.noreply.github.com>
2021-10-28 16:34:17 +0800

    Used the student class on DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
@@ -15,7 +18,17 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 	@GetMapping
-	public List<String> hello(){
-		return List.of("ZA", "WARUDO");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Dio",
+						LocalDate.of(1868, Month.APRIL,12),
+						"kono.dio@gmail.com",
+						121
+
+
+				)
+		);
 	}
 }
